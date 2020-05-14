2328da67772e66c69124076363bfc49274f9abb5
Shiloli-r <45619599+Shiloli-r@users.noreply.github.com>
2020-05-14 04:28:37 +0300

    Added requirements.txt

--- motorhub/views.py
+++ motorhub/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import SearchForm
 
 
 # Create your views here.
@@ -6,3 +9,11 @@ def home(request):
     return render(request, 'base.html')
 
 
+def get_name(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/index.html')
+    else:
+        form = SearchForm()
+    return render(request, 'base.html', {"form": form})
